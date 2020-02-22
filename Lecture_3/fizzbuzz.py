6b6e6de3469ae1d2d059e74bafd9ac11e7257573
Inhwan Lee <lih0905@gmail.com>
2020-02-22 23:54:00 +0900

    Lecture 3 practice

---
+++ Lecture_3/fizzbuzz.py
@@ -0,0 +1,18 @@
+import sys
+
+def fizz_buzz(limit):
+    for i in range(1,limit+1):
+        if i % 3 == 0:
+            print('fizz')
+        if i % 5 == 0:
+            print('fizz')
+        if i % 3 and i % 5:
+            print(i)
+        if i % 15 == 0:
+            print('fizz\nbuzz')
+
+def main(num):
+    fizz_buzz(num)
+
+if __name__== '__main__':
+    main(int(sys.argv[1]))
